ac681f3e37a10f085e33570ad420237d84f3a144
Gonçalo Pinto <69385490+gsespinto@users.noreply.github.com>
2024-10-22 15:01:58 +0100

    Add LoadingScreen autoload

---
+++ 01_scripts/ui/loading_screen.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+@export var animator : AnimationPlayer
+var _scene_to_load : String = ""
+
+
+func _ready():
+	if not is_instance_valid(animator):
+		return
+	
+	animator.animation_finished.connect(_on_animation_finished)
+
+
+func load_scene(file_path : String):
+	if file_path.is_empty():
+		return
+	
+	_scene_to_load = file_path
+	animator.play("load")
+	show()
+
+
+func _start_loading_scene():
+	get_tree().change_scene_to_file(_scene_to_load)
+	animator.play("unload")
+
+
+func _on_animation_finished(animation : String):
+	match animation:
+		"unload":
+			hide()
