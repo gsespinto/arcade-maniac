1d16a320a8a5bbc1d93798bf29e0cd7380dc2fcd
Gonçalo Pinto <69385490+gsespinto@users.noreply.github.com>
2024-11-09 19:24:49 +0000

    Add chronometer arm

--- 01_scripts/character_viewport.gd
+++ 01_scripts/character_viewport.gd
@@ -1,12 +1,16 @@
 extends Node3D
 class_name CharacterViewport
 
-@export var animator : AnimationPlayer
+@export var remote_arm_animator : AnimationPlayer
+@export var timer_arm_animator : AnimationPlayer
 @export var blend_time : float = 0.05
 
 
 func play_animation(animation : String) -> void:
-	if not is_instance_valid(animator):
-		return
+	if is_instance_valid(remote_arm_animator) \
+			and remote_arm_animator.has_animation(animation):
+		remote_arm_animator.play(animation, blend_time)
 	
-	animator.play(animation, blend_time)
+	if is_instance_valid(timer_arm_animator) \
+			and timer_arm_animator.has_animation(animation):
+		timer_arm_animator.play(animation, blend_time)
